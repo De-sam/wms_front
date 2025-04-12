12fb23e33c7730d9ea6f433ab86776411d38dd5b
henry-meboy <henryiheonu5@gmail.com>
2025-04-12 23:25:03 +0100

    landing page style begins

--- workspace-frontend/src/App.jsx
+++ workspace-frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // Adjust the path to point to the components folder
-import LandingPage from './components/Landing_page';
+import LandingPage from './components/LandingPage';
 
 const App = () => {
   return (
